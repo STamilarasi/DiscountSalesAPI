3ec74d9563b0e04a39131f546d9fbbb7dfcd7a76
Tamilarasi <tamilarasi.dhana@gmail.com>
2020-07-07 14:04:08 +0530

    Added DiscountSales Model.

--- DiscountSalesAPI/Models/DiscountSalesModel.cs
+++ DiscountSalesAPI/Models/DiscountSalesModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DiscountSalesAPI.Models
 {
+  
     public class DiscountSalesModel
     {
+        [JsonProperty("Id")]
+        public string Identifier { get; set; }
+
+        [JsonProperty("Type")]
+        public string Type { get; set; }
+
+        [JsonProperty("Product")]
+        public Dictionary<string, string> ProductInfo { get; set; }
+
+        [JsonProperty("Operation")]
+        public Dictionary<string, string> OperationInfo { get; set; }
+
+        [JsonProperty("Quantity")]
+        public Dictionary<string, int> QuantityInfo { get; set; }
+
+        [JsonProperty("Cost")]
+        public Dictionary<string, int> CostInfo { get; set; }
+
+    }
 
 
+    public class Discounts
+    {
+        public DiscountSalesModel[] DiscountModel { get; set; }
     }
 }
